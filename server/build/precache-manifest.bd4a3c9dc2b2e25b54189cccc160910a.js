1eb23d5fdd7dcc30c2ee87116d592c757cd1636f
Ashwal200 <Ashwal200@gmail.com>
2024-06-26 08:33:10 +0300

    WIP

---
+++ server/build/precache-manifest.bd4a3c9dc2b2e25b54189cccc160910a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc80322a138673b9548dd4ca8d77a0e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba12a509e45815adb8e8",
+    "url": "/static/css/main.ee41a919.chunk.css"
+  },
+  {
+    "revision": "bd203706fc056764a677",
+    "url": "/static/js/2.c504a010.chunk.js"
+  },
+  {
+    "revision": "beb8a887af8b88953e9f0170ab4b8b44",
+    "url": "/static/js/2.c504a010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a2293d28bc1c182226",
+    "url": "/static/js/3.bcbe5ddc.chunk.js"
+  },
+  {
+    "revision": "ba12a509e45815adb8e8",
+    "url": "/static/js/main.9ad3959b.chunk.js"
+  },
+  {
+    "revision": "db100bb7e0477e78a27e",
+    "url": "/static/js/runtime-main.9b78da3c.js"
+  }
+]);
